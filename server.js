2a8a29797835eff2c6210693a2a175c07bbc23b1
oliver2md130312-ai <oliver2md130312@gmail.com>
2026-01-01 18:38:03 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,27 +4,32 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 const io = new Server(server, { cors: { origin: "*" } });
 
-const users = {};
+const users = {}; // usuarios conectados
 
 io.on("connection", socket => {
 
+  // Usuario se une
   socket.on("join", data => {
     users[socket.id] = { id: socket.id, name: data.name, role: data.role };
     io.emit("receivers", Object.values(users).filter(u => u.role === "receive"));
   });
 
+  // Emisor solicita enviar
   socket.on("request-send", data => {
     io.to(data.to).emit("incoming-request", { from: socket.id, name: users[socket.id]?.name });
   });
 
+  // Receptor acepta solicitud
   socket.on("accept-request", data => {
     io.to(data.to).emit("request-accepted", { from: socket.id });
   });
 
+  // Señales WebRTC
   socket.on("signal", data => {
     io.to(data.to).emit("signal", data);
   });
 
+  // Usuario desconectado
   socket.on("disconnect", () => {
     delete users[socket.id];
     io.emit("receivers", Object.values(users).filter(u => u.role === "receive"));
