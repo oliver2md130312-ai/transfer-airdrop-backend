fa95181e7696173ab38130ea07cd31111521faf5
oliver2md130312-ai <oliver2md130312@gmail.com>
2026-01-01 18:30:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,42 +4,27 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 const io = new Server(server, { cors: { origin: "*" } });
 
-const users = {}; // usuarios conectados
+const users = {};
 
 io.on("connection", socket => {
 
-  // Usuario se une
   socket.on("join", data => {
-    users[socket.id] = {
-      id: socket.id,
-      name: data.name,
-      role: data.role
-    };
-    // Emitimos receptores a todos
+    users[socket.id] = { id: socket.id, name: data.name, role: data.role };
     io.emit("receivers", Object.values(users).filter(u => u.role === "receive"));
   });
 
-  // Emisor solicita enviar
   socket.on("request-send", data => {
-    io.to(data.to).emit("incoming-request", {
-      from: socket.id, // emisorID
-      name: users[socket.id]?.name
-    });
+    io.to(data.to).emit("incoming-request", { from: socket.id, name: users[socket.id]?.name });
   });
 
-  // Receptor acepta solicitud
   socket.on("accept-request", data => {
-    io.to(data.to).emit("request-accepted", {
-      from: socket.id // receptorID
-    });
+    io.to(data.to).emit("request-accepted", { from: socket.id });
   });
 
-  // Señales WebRTC
   socket.on("signal", data => {
     io.to(data.to).emit("signal", data);
   });
 
-  // Desconexión
   socket.on("disconnect", () => {
     delete users[socket.id];
     io.emit("receivers", Object.values(users).filter(u => u.role === "receive"));
